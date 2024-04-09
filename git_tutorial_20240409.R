a1a5df535a26f0f565f647e248d569ced460db63
Megan Podolinsky <148594737+meganpodolinsky@users.noreply.github.com>
2024-04-09 13:00:50 -0600

    added new R file

---
+++ git_tutorial_20240409.R
@@ -0,0 +1,14 @@
+#coding and cookies workshop - versioning with git
+#April 9 2024
+install.packages("gitcreds")
+
+library(gitcreds)
+
+gitcreds::gitcreds_set()
+#mine is already linked to my git credentials
+#no need for token
+
+#now the repository is locally cloned onto my computer
+
+x <- rnorm(100)
+hist(x)
